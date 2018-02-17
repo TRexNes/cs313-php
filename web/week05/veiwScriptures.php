e23e9ba898458ee1ed87f07681a164b76eb0b87d
T-Rex <nes14001@byui.edu>
2018-02-17 15:22:03 -0700

    added week05

--- web/week05/veiwScriptures.php
+++ web/week05/veiwScriptures.php
@@ -1,7 +1,35 @@
+<?php
+/**********************************************************
+* File: viewScriptures.php
+* Author: Br. Burton
+* 
+* Description: This file shows an example of how to query a
+*   PostgreSQL database from PHP.
+***********************************************************/
+// It would be better to store these in a different file
+$dbUser = 'postgres';
+$dbPassword = 'avsnut';
+$dbName = 'postgres';
+$dbHost = 'localhost';
+$dbPort = '5432';
+try
+{
+	// Create the PDO connection
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch (PDOException $ex)
+{
+	// If this were in production, you would not want to echo
+	// the details of the exception.
+	echo "Error connecting to DB. Details: $ex";
+	die();
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
-
 	<title>Scripture List</title>
 </head>
 
@@ -10,45 +38,33 @@
 
 <h1>Scripture Resources</h1>
 
-     <?php
-      
-     
-         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        
-                        $var = $_POST["search1"];
-      
-            
-            //********************************************************
-            include("dbOpen.php");
-            //********************************************************
-            
-  echo '<table id="scripture">';
-               foreach ($db->query("
-                  SELECT scripture_id, book, chapter, verse, content 
-                  FROM public.scriptures  
-                  WHERE book LIKE '%$var%' ") as $row)
-                  {
-                    echo '<tr id="sql_data">';
-                     echo '<td> <a href="week05ta_4.php?id='.$row[scripture_id].'"><b>' .$row[book].' '. $row[chapter].':' .$row[verse]. '</b></a> </td>';
-                     echo '</tr>';   /**/
-                  }
-            
-            echo '</table>';
- 
-     
-      
-       } else {
-                           echo "This is embarrassing - - - There was an error in the REQUEST_METHOD not being POSTed. . . ";
-                        }
-           include("dbClose.php");
-      
-      ?>
-
-    </div>
-   
-   
-</body>
-</html>  
+<?php
+// In this example, for simplicity, the query is executed
+// right here and the data echoed out as we iterate the query.
+// You could imagine that in a more involved application, we
+// would likely query the database in a completely separate file / function
+// and build a list of objects that held the components of each
+// scripture. Then, here on the page, we could simply call that 
+// function, and iterate through the list that was returned and
+// print each component.
+// First, prepare the statement
+// Notice that we avoid using "SELECT *" here. This is considered
+// good practice so we don't inadvertently bring back data we don't
+// want, especially if the database changes later.
+$statement = $db->prepare("SELECT book, chapter, verse, content FROM scripture");
+$statement->execute();
+// Go through each result
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	// The variable "row" now holds the complete record for that
+	// row, and we can access the different values based on their
+	// name
+	echo '<p>';
+	echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':';
+	echo $row['verse'] . '</strong>' . ' - ' . $row['content'];
+	echo '</p>';
+}
+?>
 
 
 </div>

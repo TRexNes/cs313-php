fa9dfdf685aa0a2649d37a12fcd20b5b196f20bf
T-Rex <nes14001@byui.edu>
2018-02-22 13:05:02 -0700

    added week07

--- web/week07/Player&StatEntry.php
+++ web/week07/Player&StatEntry.php
@@ -94,7 +94,7 @@ try
 		echo '<p>';
 		echo 'Player Name: '. $row['last_name'];
 		echo '<br />';
-		echo 'Games Played: '. $row['games_played'] . ' Goals: ' . $row['goals'] . ' Assists: ';
+		echo 'Games Played: '. ' ' . $row['games_played'] . ' Goals: ' . $row['goals'] . ' Assists: ';
 		echo $row['assists'] . ' Points: ' . $row['points'] . ' Penalty Mins: ';
 		echo $row['penalty_mins'] . '</strong>';
 		echo '<br />';
